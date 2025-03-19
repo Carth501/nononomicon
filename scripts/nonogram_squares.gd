a7ab22f5b047899fcbbc90e402adc80c382b7ead
carth501 <carth501@gmail.com>
2025-03-19 18:42:56 -0400

    Revert "Add cheat button and enhance square display management"
    This reverts commit 2096b62d5af1a48a22b3309c1d0c74ad347aafda.

--- scripts/nonogram_squares.gd
+++ scripts/nonogram_squares.gd
@@ -1,19 +1,12 @@
 class_name NonogramSquares extends GridContainer
 
 var square_scene = preload("res://scenes/square.tscn")
-var squares = []
 
-func create_square_displays(square_map: Dictionary):
-	if (squares.size() > 0):
-		for i in squares:
-			i.queue_free()
-		squares = []
-	var map = square_map
+func create_square_displays():
+	var map = State.master[State.SQUARE_MAP_KEY]
 	columns = map.keys().size()
 	for i in map.keys().size():
 		for k in map[0].size():
 			var new_square = square_scene.instantiate()
 			new_square.setup(Vector2i(i, k))
 			add_child(new_square)
-			squares.append(new_square)
-			new_square.set_square_appearance(map[i][k])
--- scripts/y_header_col.gd
+++ scripts/y_header_col.gd
@@ -2,15 +2,9 @@ class_name YHeaderCol extends Control
 
 @export var header_row_container: VBoxContainer
 var y_header_cell_scene = preload("res://scenes/y_header_cell.tscn")
-var cells = []
 
 func generate_cells(values: Dictionary):
-	if (cells.size() > 0):
-		for i in cells:
-			i.queue_free()
-		cells = []
 	for i in values.keys():
 		var new_cell = y_header_cell_scene.instantiate()
 		new_cell.generate_labels(values[i])
 		header_row_container.add_child(new_cell)
-		cells.append(new_cell)
