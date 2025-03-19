2096b62d5af1a48a22b3309c1d0c74ad347aafda
carth501 <carth501@gmail.com>
2025-03-19 17:47:05 -0400

    Add cheat button and enhance square display management

--- scripts/x_header_row.gd
+++ scripts/x_header_row.gd
@@ -2,9 +2,15 @@ class_name XHeaderRow extends Control
 
 @export var header_row_container: HBoxContainer
 var x_header_cell_scene = preload("res://scenes/x_header_cell.tscn")
+var cells = []
 
 func generate_cells(values: Dictionary):
+	if (cells.size() > 0):
+		for i in cells:
+			i.queue_free()
+		cells = []
 	for i in values.keys():
 		var new_cell = x_header_cell_scene.instantiate()
 		new_cell.generate_labels(values[i])
 		header_row_container.add_child(new_cell)
+		cells.append(new_cell)
