bb428f2e6fe5380b3c26eeb5c54b082797aa29d3
carth501 <carth501@gmail.com>
2025-03-21 22:49:30 -0400

    Add unit tests for shared end segments and danger square resolution

--- test/unit/test_example.gd
+++ test/unit/test_example.gd
@@ -39,12 +39,35 @@ class TestStateFunctions:
 		State.generate_headers()
 
 	func test_get_duplicate_lengths():
-		var axis = 'X'
-		var lengths = State.get_duplicate_lengths(axis)
-		print(lengths)
+		var lengths = State.get_duplicate_lengths('X')
 		assert_true(lengths != {}, "Should have found some duplicates")
 
 
 	func test_find_offset_function():
 		var x_offset_segments = State.find_offset_by_one_segments('X')
 		assert_true(x_offset_segments == {"1_0_1": {"segment1": [1], "segment2": [0], "index1": 0, "index2": 1}}, "Should have found the offset segments")
+
+	func test_find_shared_end_segments():
+		var x_offset_segments = State.find_offset_by_one_segments('X')
+		var y_offset_segments = State.find_offset_by_one_segments('Y')
+		var shared_end_segments = State.find_shared_end_segments(x_offset_segments, y_offset_segments)
+		print("shared_end_segments ", shared_end_segments)
+		assert_true(shared_end_segments.size() > 0, "Should have found shared end segments")
+		assert_true(shared_end_segments[0].has('x'), "Shared segment should have 'x' key")
+		assert_true(shared_end_segments[0].has('y'), "Shared segment should have 'y' key")
+		assert_true(shared_end_segments[0].has('unmarked_corners'), "Shared segment should have 'unmarked_corners' key")
+		assert_true(shared_end_segments[0]['x'] == {"segment1": [1], "segment2": [0], "index1": 0, "index2": 1}, "Should have found the x points")
+		assert_true(shared_end_segments[0]['y'] == {"segment1": [1], "segment2": [0], "index1": 0, "index2": 1}, "Should have found the y points")
+		var point1 = Vector2i(0, 0)
+		var point2 = Vector2i(1, 1)
+		assert_true(shared_end_segments[0]['unmarked_corners'] == [point1, point2], "Should have found the unmarked corners")
+
+	func test_resolve_danger_square():
+		var x_offset_segments = State.find_offset_by_one_segments('X')
+		var y_offset_segments = State.find_offset_by_one_segments('Y')
+		var shared_end_segments = State.find_shared_end_segments(x_offset_segments, y_offset_segments)
+		var changed_points = State.resolve_danger_square(shared_end_segments)
+		print("changed_points ", changed_points)
+		var point = Vector2i(0, 1)
+		assert_true((changed_points == [point]), "Should have changed the point [0, 1] to empty")
+		assert_true(State.get_target_position(point) == State.SquareStates.EMPTY, "The changed point should be empty")
