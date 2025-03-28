9cd89c67ed92e58582b54706f4655c59a6db8305
carth501 <carth501@gmail.com>
2025-03-28 15:36:57 -0400

    Remove unused ScrollContainerWrapper script to clean up codebase

--- scripts/scroll_container_wrapper.gd
+++
@@ -1,9 +0,0 @@
-# class_name ScrollContainerWrapper extends ScrollContainer
-
-# func get_percent_x() -> float:
-# 	var max_x = get_h_scroll_bar().max_value
-# 	return float(scroll_horizontal) / max_x
-
-# func get_percent_y() -> float:
-# 	var max_y = get_v_scroll_bar().max_value
-# 	return float(scroll_vertical) / max_y
