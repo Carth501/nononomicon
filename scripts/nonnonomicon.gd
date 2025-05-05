428ede3779ad9d6ab4a4e83130e30b48795b7ee8
carth501 <carth501@gmail.com>
2025-05-05 14:41:44 -0400

    Hide the hint button instead of disabling it

--- scripts/nonnonomicon.gd
+++ scripts/nonnonomicon.gd
@@ -186,12 +186,11 @@ func hide_power_description():
 	power_description_container.hide()
 	descriptions.show()
 
-
 func _on_main_menu_button_pressed() -> void:
 	get_tree().change_scene_to_file("res://scenes/main_menu.tscn")
 
 func set_hint_button():
-	hint_button.disabled = !State.has_hints()
+	hint_button.visible = !State.has_hints()
 
 func _on_hint_button_pressed() -> void:
 	State.request_hint()
