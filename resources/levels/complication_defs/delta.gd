bd51df3074d21c3f372cf8979fe1740b6ef56b1d
carth501 <carth501@gmail.com>
2025-05-20 13:10:12 -0400

    set the type variable in complications

--- resources/levels/complication_defs/delta.gd
+++ resources/levels/complication_defs/delta.gd
@@ -4,3 +4,6 @@ class_name DeltaComplication extends Complication
 @export var delta_subject_row: int = -1
 @export var delta_variable_column: int = -1
 @export var delta_variable_row: int = -1
+
+func _init() -> void:
+	type = "delta"
--- resources/levels/complication_defs/variable.gd
+++ resources/levels/complication_defs/variable.gd
@@ -2,3 +2,6 @@ class_name Variable extends Complication
 
 @export var value: String
 @export var glyph: String
+
+func _init() -> void:
+	type = "variable"
